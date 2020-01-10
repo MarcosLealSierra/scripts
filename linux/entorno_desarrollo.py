a10f78cc2faaada47ab1ed676151129bb713c06e
Marcos Leal Sierra <marcoslealsierra90@gmail.com>
2020-01-10 13:25:31 +0100

    get files and application tab

--- linux/entorno_desarrollo.py
+++ linux/entorno_desarrollo.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from os import listdir, path
+import os
 from shlex import split as sh_split
-from subprocess import call, Popen, PIPE
+from subprocess import call
 from sys import argv
 
 
-def get_files(target):
-    archivos = []
-    for fname in listdir(target):
-        if path.isdir("{}/{}".format(target, fname)):
-            continue
-        else:
-            archivos.append(fname)
-    if not "static" in target:
-        archivos.remove("__init__.py")
-    return archivos
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in dirs:
+            return os.path.join(root, name)
 
-proyecto = "{}".format(argv[1])
-
-target = "/home/mleal/src/proyectos/{}".format(proyecto)
+proyecto = "{}".format(argv[1]).replace("/", "").replace(" ", "")
+target = find(proyecto, '/home/mleal/src')
 application = "{}/rootsystem/application".format(target)
 modules = "{}/modules".format(application)
 static= "{}/rootsystem/static".format(target)
@@ -32,31 +25,21 @@ vim = "cd {}; vim ."
 edit = "cd {}; vim -c"
 interactive = "; bash -i"
 
-application_files = get_files(application)
-files_application = "e {}".format(application_files[0])
-application_files.pop(0)
-for archivo in application_files:
-    files_application += "|tabe {}".format(archivo)
-panel_application = '{} "{} \'{}\'{}"'.format(
-    panel, 
-    edit.format(application), 
-    files_application,
-    interactive
-)
+panel_application = '{} "{}"'.format(panel, vim.format(application))
 panel_modules = '{} "{}"'.format(panel, vim.format(modules))
 panel_core = '{} "{}"'.format(panel, vim.format(core))
 panel_static = '{} "{}"'.format(panel, vim.format(static))
 panel_logs = '{} "{}"'.format(panel, plogs)
 
 comandos = [
-    "tmux new-session -A -d -s {}".format(proyecto),
+    "tmux new-session -A -d -s {} -c {} -n repo".format(proyecto, application),
     panel_application.replace("seccion", "application"),
     panel_modules.replace("seccion", "modules"),
     panel_core.replace("seccion", "core"),
     panel_static.replace("seccion", "static"),
     panel_logs.replace("seccion", "logs"),
     "tmux set-option -g pane-active-border-fg colour240",
-    "tmux attach -t {}".format(proyecto)
+    "tmux attach -t {}:1".format(proyecto, application)
 ]
 
 for indice, comando in enumerate(comandos):
