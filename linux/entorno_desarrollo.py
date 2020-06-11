34f800f625cfc25b4070c02fbc1c5d186cf463f5
Marcos Leal Sierra <marcoslealsierra90@gmail.com>
2020-06-11 11:29:32 +0200

    misc

--- linux/entorno_desarrollo.py
+++ linux/entorno_desarrollo.py
@@ -12,15 +12,15 @@ def find(name, path):
         if name in dirs:
             return os.path.join(root, name)
 
-proyecto = "{}".format(argv[1]).replace("/", "").replace(" ", "")
+proyecto = "{}".format(argv[1]).replace("@", "").replace("/", "").replace(" ", "")
 target = find(proyecto, '/home/mleal/src')
 application = "{}/rootsystem/application".format(target)
 modules = "{}/modules".format(application)
 static= "{}/rootsystem/static".format(target)
 core = "{}/core".format(application)
-logs = "/srv/websites/{}/logs".format(proyecto)
+logs = "{}/logs".format(target)
 panel = "tmux new-window -t {} -n seccion".format(proyecto)
-plogs = "cd /srv/websites/{}/logs; tail -f error.log | grep -oP '(?<=AH01215:).*'".format(proyecto)
+plogs = "cd {}; tail -f error.log | grep -oP '(?<=AH01215:).*'".format(logs)
 vim = "cd {}; vim ."
 edit = "cd {}; vim -c"
 interactive = "; bash -i"
@@ -33,13 +33,13 @@ panel_logs = '{} "{}"'.format(panel, plogs)
 
 comandos = [
     "tmux new-session -A -d -s {} -c {} -n repo".format(proyecto, application),
+    panel_logs.replace("seccion", "logs"),
     panel_application.replace("seccion", "application"),
     panel_modules.replace("seccion", "modules"),
     panel_core.replace("seccion", "core"),
     panel_static.replace("seccion", "static"),
-    panel_logs.replace("seccion", "logs"),
     "tmux set-option -g pane-active-border-fg colour240",
-    "tmux attach -t {}:1".format(proyecto, application)
+    "tmux attach -t {}:0".format(proyecto, application)
 ]
 
 for indice, comando in enumerate(comandos):
