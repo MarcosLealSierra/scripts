253d7a2edd4906bae5b5915c66dbf0e7bc346383
Marcos Leal Sierra <marcoslealsierra90@gmail.com>
2020-06-11 11:31:45 +0200

    misc

--- linux/pctj.py
+++ linux/pctj.py
@@ -5,10 +5,10 @@ from sys import argv
 
 
 def pctj(porcentaje = argv[1], cantidad = argv[2]):
-    
+
     val_pctj = int(cantidad) * int(porcentaje) / 100
     total_cantidad = int(cantidad) - val_pctj
-    
+
     print("{}% sobre {}".format(porcentaje, cantidad))
     print("Descuento: {}".format(val_pctj))
     print("Resultado: {}".format(total_cantidad))
